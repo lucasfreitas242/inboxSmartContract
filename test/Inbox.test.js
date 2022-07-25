fc37b42755209af25279c9bba04702af7522552e
Lucas Freitas <lucas@xsys.com.br>
2022-07-25 17:40:50 -0300

    Implementação dos testes diretamente sobre o contrato Inbox em uma rede local.

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -8,19 +8,36 @@ const web3 = new Web3(ganache.provider());
 let accounts;
 let inbox;
 
+const INITIAL_MESSAGE = 'olá, Criptomundo!';
+const CHANGED_MESSAGE = 'tchau, Criptomundo!';
+
 beforeEach(async () => {
    accounts = await web3.eth.getAccounts();
 
    inbox = await new web3.eth.Contract(abi)
       .deploy({
          data: evm.bytecode.object,
-         arguments: ['olá, Criptomundo!'],
+         arguments: [INITIAL_MESSAGE],
       })
-      .send({ from: accounts[0], gas: '1000000' })
+      .send({ from: accounts[0], gas: '1000000' });
 });
 
 describe('Inbox', () => {
    it('deploys a contract', () => {
-      console.log(inbox);
+      assert.ok(inbox.options.address);
+   });
+
+   it('has a default message', async () => {
+      const message = await inbox.methods.message().call();
+      assert.equal(message, INITIAL_MESSAGE);
+      console.log(message);
+   })
+
+   it('can change the message', async () => {
+      await inbox.methods.setMessage(CHANGED_MESSAGE).send({ from: accounts[0]});
+      const message = await inbox.methods.message().call();
+      assert.equal(message, CHANGED_MESSAGE);
+      console.log(message);
+
    })
 })
